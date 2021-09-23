8ee75fe8fe0573c11904cb946bf211276c0b866d
Oleksandr Kuznetsov <burn0354@algonquinlive.com>
2021-09-23 12:55:28 -0400

    Update infoslideover.dart
    added changes to infopage UI

--- lib/infoslideover.dart
+++ lib/infoslideover.dart
@@ -18,7 +18,84 @@ class _InfoPageState extends State<InfoPage> {
           title: Text('Info Page'),
           centerTitle: true,
           backgroundColor: Colors.transparent),
-      body: Center(),
+      body: Center(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Center(
+                child: Text(
+                  'MasjidPass',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24
+                  ),
+                ),
+              ),
+              onTap: () => null,
+            ),
+            ListTile(
+              title: Center(
+                child: Text(
+                  'System Information',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Device ID',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  'f774690826290hd832'
+              ),
+              onTap: () => null,
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text(
+                'Scanner Version',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  '2.8'
+              ),
+              onTap: () => null,
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text(
+                'Authentication API Version',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  '1.0'
+              ),
+              onTap: () => null,
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text(
+                'Backend API Version',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  '1.0'
+              ),
+              onTap: () => null,
+            ),
+            Divider(
+              thickness: 2,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
