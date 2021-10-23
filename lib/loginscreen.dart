0a278951c8655cb4ab94da86eea34865c8118083
Mariano velasco <mvela022@gmail.com>
2021-10-22 22:42:18 -0400

    Final commit

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -1,155 +1,176 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:masjid_pass/settingspage.dart';
-import 'package:form_field_validator/form_field_validator.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: LoginPage(),
-  ));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MaterialApp(home: LoginPage()));
 }
 
-// ignore: use_key_in_widget_constructors
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
-  _State createState() => _State();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _State extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
   GlobalKey<FormState> form = GlobalKey<FormState>();
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  String username = '';
+  Color backColor = Colors.white;
+  String password = '';
+  final message2 = "masjidPass2021";
+  String wrongCreds = "";
 
   _navigateToSettings() async {
-    await Future.delayed(Duration(milliseconds: 500));
+    await Future.delayed(const Duration(milliseconds: 500));
     Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-            builder: (context) => SettingsPage(
+            builder: (context) => const SettingsPage(
                   title: "Settings Page",
                 )));
   }
 
-  void validate() {
-    if (form.currentState!.validate()) {
-      //  validate ()=>logingPressed();
+  bool credentials(value) {
+    if (value != message2) {
+      return false;
     } else {
-      print("Not validated");
-    }
-  }
-
-  String validation(value) {
-    if (value!.isEmpty) {
-      return "Required";
-    } else {
-      return "";
+      return true;
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(30),
-        child: Center(
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
           child: Form(
-            autovalidateMode: AutovalidateMode.always,
             key: form,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Icon(
-                    Icons.qr_code_scanner,
-                    color: Colors.blue,
-                    size: 100.0,
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "Masjid Check-in Scanner",
-                    textAlign: TextAlign.center,
-                    textScaleFactor: 2.0,
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-                Flexible(
-                  flex: 2,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'User Name'),
-                    validator: validation,
-                  ),
-                ),
-                Flexible(
-                  flex: 2,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Password'),
-                    validator: validation,
-                  ),
-                ),
-                Padding(
-                    padding: EdgeInsets.fromLTRB(
-                      10,
-                      10,
-                      0,
-                      0,
-                    ),
-                    child: ElevatedButton(
-                      onPressed:
-                          _loginPressed, // showAlertDialog(context), //validate,
-                      child: Text('Login'),
-                    )),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: ListView(
+              padding: const EdgeInsets.all(30),
+              children: [
+                buildLabel(),
+                buildText(),
+                buildUsername(),
+                const SizedBox(height: 16),
+                buildPassword(),
+                const SizedBox(height: 32),
+                buildSubmit(),
               ],
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 
-  /* showAlertDialog(BuildContext context) {
-    // set up the buttons
-    Widget remindButton = TextButton(
-      child: Text("While using the App"),
-      onPressed: () {},
-    );
-    Widget cancelButton = TextButton(
-      child: Text("Only this time"),
-      onPressed: () {},
-    );
-    Widget launchButton = TextButton(
-      child: Text("Deny"),
-      onPressed: () {},
-    );
-    // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: Text(""),
-      content: Text(
-          "Allow Masjid Pass Check-in Scanner to access this device's location ? "),
-      actions: [
-        remindButton,
-        cancelButton,
-        launchButton,
-      ],
-    );
-    // show the dialog
-    //showDialog(
-    // context: context,
-    // {
-    //  return alert;
-    //},
-    // );
-  }
-*/
-  void progressIndicator() {
-    CircularProgressIndicator();
-    Future.delayed(const Duration(seconds: 20));
-  }
+  Widget buildLabel() => Container(
+        padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+        child: const Icon(
+          Icons.qr_code_scanner,
+          color: Colors.blue,
+          size: 80.0,
+        ),
+      );
+  Widget buildText() => Container(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+        child: const Text(
+          "Masjid Check-in Scanner",
+          textAlign: TextAlign.center,
+          textScaleFactor: 2.0,
+          style: TextStyle(
+              color: Colors.blue, fontSize: 10.0, fontFamily: "Arial"),
+        ),
+      );
+  Widget buildUsername() => TextFormField(
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(12),
+        ],
+        maxLengthEnforcement: MaxLengthEnforcement.enforced,
+        decoration: const InputDecoration(
+          labelText: 'Username',
+          border: OutlineInputBorder(),
+        ),
+        /*validator: (value) {
+          if (value.length < 4) {
+            return 'Enter at least 4 characters';
+          } else {
+            return null;
+          }
+        },
+        maxLength: 30,
+        */
+        onSaved: (value) => setState(() => username = value!),
+      );
 
+  Widget buildPassword() => TextFormField(
+        decoration: const InputDecoration(
+          labelText: 'Password',
+          border: OutlineInputBorder(),
+        ),
+        validator: (value) {
+          /*  if (value.length < 7) {
+            return 'Password must be at least 7 characters long';
+          } else {
+            return null;
+          }
+          */
+        },
+        onSaved: (value) => setState(() => password = value!),
+        keyboardType: TextInputType.visiblePassword,
+        obscureText: true,
+      );
+
+  Widget buildSubmit() => Builder(
+        builder: (context) => ButtonWidget(
+            text: 'Login',
+            onClicked: () {
+              wrongCreds = "Login Succesful";
+              final isValid = form.currentState!.validate();
+              // FocusScope.of(context).unfocus();
+              if (isValid) {
+                form.currentState!.save();
+                String message = '$username$password';
+                if (credentials(message) == true) {
+                  _loginPressed();
+                } else {
+                  wrongCreds = "Wrong Credentials";
+                }
+              }
+              final snackBar = SnackBar(
+                content: Text(
+                  wrongCreds,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                backgroundColor: Colors.green,
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }),
+      );
   void _loginPressed() {
     _navigateToSettings();
   }
+}
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    required this.text,
+    required this.onClicked,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 20),
+        ),
+        onPressed: onClicked,
+      );
 }
